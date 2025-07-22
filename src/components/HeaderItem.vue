dcc3f25be129a3d05a4daa0ef2cb1c374a709f4d
hunter <shining3721@126.com>
2025-07-22 15:43:34 +0800

    将登录后的样式修改为下拉菜单，但有黑框问题存在

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -2,7 +2,8 @@
 import { computed, ref } from 'vue'
 import { useRouter,useRoute } from 'vue-router'
 import { useUserStore } from '@/stores/user'
-import { ElNotification } from 'element-plus';
+import { ElMessageBox, ElNotification } from 'element-plus';
+import { SwitchButton, ArrowDown } from '@element-plus/icons-vue'
 
 const userStore = useUserStore();
 const activeName = computed(() => {
@@ -45,6 +46,26 @@ const handleSelect = (key: string) => {
   router.push(`/${key}`);
   (document.activeElement as HTMLElement)?.blur();
 }
+const onCommand = async (toPath: string | number | object) => {
+
+  if(!userStore.token){
+    ElNotification({
+      type: 'error',
+      message: '您还没有登录'
+    });
+    return;
+  }
+  if (toPath === 'logout') {
+    await ElMessageBox.confirm('确认退出系统吗？', '温馨提示', {
+      type: 'warning',
+      confirmButtonText: '确认',
+      cancelButtonText: '取消'
+    })
+    userStore.userLogout();
+  } else {
+    router.push(`/${toPath}`)
+  }
+}
 </script>
 
 <template>
@@ -61,8 +82,21 @@ const handleSelect = (key: string) => {
       <el-menu-item index="problemset">题目</el-menu-item>
       <el-menu-item index="contest">比赛</el-menu-item>
     </el-menu>
-    <el-row style="padding: 12px">
-      <el-button round v-if="userStore.token" @click="handleLogout">退出登录</el-button>
+    <el-row>
+      <el-dropdown v-if="userStore.token" placement="bottom-end" @command="onCommand">
+        <span class="el-dropdown-link">
+          Dropdown List<el-icon class="el-icon--right"><arrow-down /></el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item command="profile">个人资料</el-dropdown-item>
+            <el-dropdown-item command="password">修改密码</el-dropdown-item>
+            <el-dropdown-item command="admin">后台管理</el-dropdown-item>
+            <el-dropdown-item command="logout" :icon="SwitchButton">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+      <!-- <el-button round v-if="userStore.token" @click="handleLogout">退出登录</el-button> -->
       <el-button round v-else @click="handleLogin">登录</el-button>
       <el-button round v-if="!userStore.token" @click="handleRegister">注册</el-button>
     </el-row>
@@ -88,5 +122,11 @@ const handleSelect = (key: string) => {
 .nav-menu {
   flex-grow: 1;
   border-bottom: none;
+}
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
 }
 </style>
