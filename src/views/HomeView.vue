6c5784749ac9893d472b65e6110e80de20496db7
hunter <shining3721@126.com>
2025-07-22 19:26:06 +0800

    主页添加公告标签卡

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,63 @@
 <script setup lang="ts">
-
+import MainCard from '@/components/MainCard.vue'
 </script>
 
 <template>
-  home
+  <MainCard class="announcement_card">
+    <template #title>
+      <span>公告</span>
+    </template>
+    <template #extra>
+      <el-link>更多</el-link>
+    </template>
+    <template #content>
+      <div class="announcement_list">
+        <el-row>
+          <span>公告标题</span>
+        </el-row>
+        <el-row>
+          <span>发稿人</span>
+        </el-row>
+      </div>
+      <div class="announcement_list">
+        <el-row>
+          <span>公告标题</span>
+        </el-row>
+        <el-row>
+          <span>发稿人</span>
+        </el-row>
+      </div>
+
+      <div class="announcement_list">
+        <el-row>
+          <span>公告标题</span>
+        </el-row>
+        <el-row>
+          <span>发稿人</span>
+        </el-row>
+      </div>
+
+      <div class="announcement_list">
+        <el-row>
+          <span>公告标题</span>
+        </el-row>
+        <el-row>
+          <span>发稿人</span>
+        </el-row>
+      </div>
+    </template>
+  </MainCard>
 </template>
 
 <style scoped>
-
+.announcement_card{
+  margin-left: 150px;
+  margin-right: 150px;
+}
+.announcement_list{
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 2px solid #eaeaea;
+  padding: 10px;
+}
 </style>
